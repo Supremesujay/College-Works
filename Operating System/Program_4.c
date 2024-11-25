8c93430d1c70f121ec7c6ec2d9d79a3b0e5cf6fa
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2024-11-25 23:28:15 +0530

    Add files via upload

---
+++ Operating System/Program_4.c
@@ -0,0 +1,45 @@
+/* Develop a C program which demonstrates inter-process communication between a
+reader process and a writer process. Use mkfifo, open, read, write and close APIs in
+your program. */
+
+/*Writer Process*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main()
+{
+    int fd;
+    char buf[1024];
+    /* create the FIFO (named pipe) */
+    char *myfifo = "/tmp/myfifo";
+    mkfifo(myfifo, 0666);
+    printf("Run Reader process to read the FIFO File\n");
+    fd = open(myfifo, O_WRONLY);
+    write(fd, "Hi", sizeof("Hi"));
+    /* write "Hi" to the FIFO */
+    close(fd);
+    unlink(myfifo); /* remove the FIFO */
+    return 0;
+}
+/* Reader Process*/
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#define MAX_BUF 1024
+int main()
+{
+    int fd;
+    /* A temp FIFO file is not created in reader */
+    char *myfifo = "/tmp/myfifo";
+    char buf[MAX_BUF];
+    /* open, read, and display the message from the FIFO */
+    fd = open(myfifo, O_RDONLY);
+    read(fd, buf, MAX_BUF);
+    printf("Writer: %s\n", buf);
+    close(fd);
+    return 0;
+}
