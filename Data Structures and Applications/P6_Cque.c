6a89f4af25c8e3f906adeb5647ec8cfc92ddf235
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2024-12-06 22:01:58 +0530

    Update P6_Cque.c

--- Data Structures and Applications/P6_Cque.c
+++ Data Structures and Applications/P6_Cque.c
@@ -1,4 +1,4 @@
-/*6.Develop a menu driven Program in C for the following operations on Circular QUEUE of Characters 
+/* 6.Develop a menu driven Program in C for the following operations on Circular QUEUE of Characters 
 (Array Implementation of Queue with maximum size MAX). 
 a. Insert an Element on to Circular QUEUE 
 b. Delete an Element from Circular QUEUE 
