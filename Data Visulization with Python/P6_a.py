537abdba6cacb817c2765e7ca4e62aa47d0db630
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2024-12-06 21:56:33 +0530

    Update P6_a.py

--- Data Visulization with Python/P6_a.py
+++ Data Visulization with Python/P6_a.py
@@ -1,4 +1,4 @@
-#Write a Python program to illustrate Linear Plotting using Matplotlib.
+# Write a Python program to illustrate Linear Plotting using Matplotlib.
 
 import matplotlib.pyplot as plt
 overs = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
